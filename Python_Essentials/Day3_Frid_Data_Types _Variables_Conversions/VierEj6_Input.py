483ea0411f08141390d869d863e15e6013699125
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 15:42:49 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/VierEj6_Input.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 19:47:09 2023
+
+@author: myepez
+"""
+#Diccionarios
+dict1={"R1":"10.10.10.1",
+       4:2,
+       "email":"correo@dominio.edu.ec",
+       "TELF":"123456789",
+       "R1":"10.10.10.2"}
+print(dict1)
+
+print(dict1["R1"])
+dict1["NOMBRE"]="MARCO"

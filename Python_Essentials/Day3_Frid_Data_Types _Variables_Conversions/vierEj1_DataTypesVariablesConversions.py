49c70f4c8c5c366f665354e23ef7e65feea347f6
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 13:10:25 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/vierEj1_DataTypesVariablesConversions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 13:05:01 2023
+
+@author: myepez
+"""
+
+x=3
+txt=" Cisco "
+resultado1=x+4
+resultado2=x//2
+opt=txt*x
+print(opt)
+print("\n"*2)
+print("\t"*8)

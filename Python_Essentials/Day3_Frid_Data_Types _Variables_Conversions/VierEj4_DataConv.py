71cd8f7aa428c89e2699fbcd19417e0de808e468
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 13:42:38 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/VierEj4_DataConv.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 13:35:41 2023
+
+@author: myepez
+"""
+
+pi=22/7
+print(pi)   #Formato por default (Flotante-Varios decimales)
+print("{:.2f}".format(pi)) #Se especifica el número de decimales a 2
+print("{:.6f}".format(pi)) #Se especifica el número de decimales a 6
+print("{:.25f}".format(pi))#Existe un límite de decimales

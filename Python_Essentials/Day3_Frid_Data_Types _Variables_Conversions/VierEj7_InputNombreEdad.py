35ad01d130cfaf002d39459094147310f6586c42
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 15:47:29 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/VierEj7_InputNombreEdad.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 20:22:06 2023
+
+@author: myepez
+"""
+
+nombre=input("Ingrese el nombre: ")
+#edad=input("Ingrese su edad: ")     #edad ingresa como string
+edad=int(input("Ingrese su edad: "))
+print(nombre)
+print(edad)

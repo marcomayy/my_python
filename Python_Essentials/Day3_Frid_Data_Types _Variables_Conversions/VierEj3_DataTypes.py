43e8043d637e6872f82857055370aa5424fe6c6d
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 13:35:24 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/VierEj3_DataTypes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 13:22:29 2023
+
+@author: myepez
+"""
+a=int()
+b=float()
+c=bool()
+d=str()
+
+x=6
+#print("El valor de X es: "+x)  #Esta línea contiene un error
+print("El valor de X es: ",x)
+
+#Funciones de conversión:
+print("El valor de X es: "+str(x))
+
+print(type(x))  #Hasta aquí, X es de tipo INT 
+x=str(x)        #Con esto cambiamos a tipo STR

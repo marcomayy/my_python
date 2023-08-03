3c2f1413fe36cd6cde72bd9069db973dbba3e9fb
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 13:54:28 -0500

    Add files via upload

---
+++ Python_Essentials/Day3_Frid_Data_Types _Variables_Conversions/VierEj5_Listas_Tuplas.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 19:20:50 2023
+
+@author: myepez
+"""
+
+tupla=(4,78.9,"R1",True)
+lista=[4,78.9,"R1",True]
+print(tupla)
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[-2])
+print(tupla[0])
+lista[2]="AP1"
+tupla[2]="AP1"

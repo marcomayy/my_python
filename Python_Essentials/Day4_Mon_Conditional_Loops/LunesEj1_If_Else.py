9f74dffc01a1af76af24b627cb666723e06e2390
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-03 16:51:24 -0500

    Add files via upload

---
+++ Python_Essentials/Day4_Mon_Conditional_Loops/LunesEj1_If_Else.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 18:43:36 2023
+
+@author: myepez
+"""
+
+datos=100
+nativa=1
+if datos==nativa:
+    print("Las VLAN son iguales")
+else:
+    print("Las VLAN son diferentes")
+    
+edad=21
+myed=18
+if edad>=myed:
+    print("Es mayor de edad")
+    

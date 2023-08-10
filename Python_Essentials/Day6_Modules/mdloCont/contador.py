e16d31fbc02b5ba1292d3116c52bb47df2fef53f
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-09 19:38:08 -0500

    Add files via upload

---
+++ Python_Essentials/Day6_Modules/mdloCont/contador.py
@@ -0,0 +1,6 @@
+def conta(n):
+    a=1
+    while a<=n:
+        print(a,end=' ')
+        a=a+1
+        print()
---
+++ Python_Essentials/Day6_Modules/mdloCont/uso_cont.py
@@ -0,0 +1,4 @@
+import contador
+#contador.conta(5)
+numer=int(input("Ingrese número a contar N: "))
+contador.conta(numer)

94dd9e1444e07ed454ff7008ad503d5ece65e1ac
marcomayy <141355614+marcomayy@users.noreply.github.com>
2023-08-09 20:22:14 -0500

    Add files via upload

---
+++ Python_Essentials/Day9_FileAccess/2LunesEj_file_access2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 20:17:08 2023
+
+@author: myepez
+"""
+
+lista=[]
+archivo=open("C:/Users/myepez/Documents/PythonDocs/devices.txt")
+for item in archivo:
+    item=item.strip("Cisco")
+    print(item)
+
+archivo.close()
+print(lista)
